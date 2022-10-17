e52557c70bbc27ac0816236c66143cb2ec0d88a9
João Pedro <joao.pedro@agxsoftware.local>
2022-10-17 17:18:44 -0300

    Fix: error on categories routes

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,15 +1,20 @@
 import { Router } from 'express'
-import { v4 as uuidV4 } from 'uuid'
 import { Category } from '../model/Category';
 
 const categoriesRoutes = Router();
 
-
+const categories: Category[] = []
 
 categoriesRoutes.post("/", (request, response) => {
     const { name, description } = request.body
 
+    const category = new Category();
     
+    category.name = name;
+    category.description = description;
+    category.created_at = new Date();
+
+    categories.push(category);
 
     return response.status(201).json({ category })
 })
